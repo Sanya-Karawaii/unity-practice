d5ce59c32833fc1b589227fd23de78bf4ec83ff2
SanyaKarawaii <KrastyAlex@yandex.ru>
2025-10-09 13:39:01 +0300

    Создан уровен для платформера с использованием компонентов типа Joints2D и Effector. Настроена работа объектов при помощи скриптов BlastController_17, SliderScript и TriggersController_17.

---
+++ Assets/Platformer/Assets/Scripts/BlastController_17r.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlastController_17 : MonoBehaviour
+{
+    public GameObject barrel;
+    public GameObject blast;
+    public GameObject blastClone;
+    public GameObject parent;
+    public GameObject Cart;
+    private PointEffector2D eff;
+    private TargetJoint2D targ;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        targ = GetComponent<TargetJoint2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnCollisionEnter2D(Collision2D obj)
+    {
+        if (obj.gameObject ==  barrel)
+        {
+            blastClone = Instantiate(blast, parent.transform);
+            barrel.SetActive(false);
+            rb.constraints = RigidbodyConstraints2D.None;
+            targ.enabled = false;
+            StartCoroutine(DestroyCart());
+            eff = blastClone.GetComponent<PointEffector2D>();
+            StartCoroutine(TurnOffEffector());
+            StartCoroutine(DestroyObject());
+
+        }
+    }
+
+    private IEnumerator TurnOffEffector()
+    {
+        yield return new WaitForSeconds(3);
+        eff.enabled = false;
+    }
+
+    private IEnumerator DestroyObject()
+    {
+        yield return new WaitForSeconds(5);
+        gameObject.SetActive(false);
+
+    }
+
+    private IEnumerator DestroyCart()
+    {
+        yield return new WaitForSeconds(5);
+        Cart.SetActive(false);
+
+    }
+}
---
+++ Assets/Platformer/Assets/Scripts/TriggersController_17.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggersController_17 : MonoBehaviour
+{
+    [SerializeField] public GameObject CartStopper;
+    private Rigidbody2D rg;
+
+    void Start()
+    {
+    }
+
+    public void OnCollisionEnter2D(Collision2D obj)
+    {
+        if (obj.gameObject == CartStopper)
+        {
+            rg = obj.gameObject.GetComponent<Rigidbody2D>();
+            rg.constraints = RigidbodyConstraints2D.None;
+            StartCoroutine(DestroyObject());
+
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.CompareTag("DeathTrigger"))
+        {
+            gameObject.SetActive(false);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    private IEnumerator DestroyObject()
+    {
+        yield return new WaitForSeconds(2);
+        CartStopper.SetActive(false);
+        
+    }
+}
