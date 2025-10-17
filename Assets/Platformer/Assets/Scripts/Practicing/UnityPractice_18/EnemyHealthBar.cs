68286a95a5481d72dba40d239251cc0017cd689d
SanyaKarawaii <KrastyAlex@yandex.ru>
2025-10-17 17:59:14 +0300

    Реализована сцена уровня Platformer: игрок может наносить урон врагам при использовании удара, враги могут наносить урон игроку при коллизии с ним, у игрока и врагов есть шкала здоровья, показывающая нанесенный параметр урона, все анимации настроены и отражают выполняемые действия.

---
+++ Assets/Platformer/Assets/Scripts/Practicing/UnityPractice_18/EnemyHealthBar.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    [SerializeField] public Slider healthBar;
+
+    public void ChangeHealthBar(float damage)
+    {
+        healthBar.value -= damage * 0.01f;
+    }
+
+
+
+}
