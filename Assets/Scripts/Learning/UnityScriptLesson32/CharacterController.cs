d2e8d60034da7a2a01cddf3bedf772a380bf600a
Sanya_Karawaii <AnnaKrasty@gmail.com>
2026-01-16 14:02:00 +0300

    Изучено применение компонента NavigationMesh.

---
+++ Assets/Scripts/Learning/UnityScriptLesson32/CharacterController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    [SerializeField] private GameObject target; 
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.transform.position;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
